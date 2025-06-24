171c4855c124174bdb3e3cfeb679a5881ca16a57
moraruCristianMarian <morarucristianmarian@yahoo.com>
2025-06-24 23:48:26 +0300

    Added form for posting game review
    [!] USER ID FOR REVIEW IS HARDCODED BECAUSE AUTHENTICATION IS NOT DONE YET

--- src/main/java/com/AWBD_Istrate_Moraru/demo/controller/ReviewController.java
+++ src/main/java/com/AWBD_Istrate_Moraru/demo/controller/ReviewController.java
@@ -2,6 +2,7 @@ package com.AWBD_Istrate_Moraru.demo.controller;
 
 import com.AWBD_Istrate_Moraru.demo.dto.ReviewDto;
 import com.AWBD_Istrate_Moraru.demo.service.ReviewService;
+import com.AWBD_Istrate_Moraru.demo.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Slf4j
@@ -17,9 +19,11 @@ import java.util.List;
 @RequestMapping("/reviews")
 public class ReviewController {
     private ReviewService reviewService;
+    private UserService userService;
 
-    public ReviewController(ReviewService reviewService) {
+    public ReviewController(ReviewService reviewService, UserService userService) {
         this.reviewService = reviewService;
+        this.userService = userService;
     }
 
     @PostMapping("")
@@ -27,6 +31,16 @@ public class ReviewController {
         reviewService.save(reviewDto);
 
         return "redirect:/reviews";
+    }
+    @PostMapping("/new")
+    public String createReview(@ModelAttribute ReviewDto reviewDto) {
+        reviewDto.setCreatedAt(LocalDate.now());
+        //  TODO
+        reviewDto.setUser(userService.findById(1L));
+
+        reviewService.save(reviewDto);
+
+        return "redirect:/games/" + reviewDto.getGame().getId();
     }
 
     @RequestMapping("")
