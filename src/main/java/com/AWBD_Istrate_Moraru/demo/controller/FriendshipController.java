65f4f50a8982a00d9734cc5851f75acb45255ca7
moraruCristianMarian <morarucristianmarian@yahoo.com>
2025-07-07 17:04:33 +0300

    Updated friends view styling
    👍

--- src/main/java/com/AWBD_Istrate_Moraru/demo/controller/FriendshipController.java
+++ src/main/java/com/AWBD_Istrate_Moraru/demo/controller/FriendshipController.java
@@ -1,9 +1,12 @@
 package com.AWBD_Istrate_Moraru.demo.controller;
 
 import com.AWBD_Istrate_Moraru.demo.dto.FriendshipDto;
-import com.AWBD_Istrate_Moraru.demo.service.FriendshipService;
+import com.AWBD_Istrate_Moraru.demo.dto.GenreDto;
+import com.AWBD_Istrate_Moraru.demo.service.*;
+import com.AWBD_Istrate_Moraru.demo.utils.ControllerReusable;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,13 +16,25 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import java.security.Principal;
 import java.util.List;
 
+@Slf4j
 @Controller
 @RequestMapping("/friendships")
-@RequiredArgsConstructor
 public class FriendshipController {
+    private GenreService genreService;
+    private UserService userService;
+    private FriendshipService friendshipService;
+    private ChatMessageService chatMessageService;
 
-    @Autowired
-    private final FriendshipService friendshipService;
+    private ControllerReusable controllerReusable;
+
+    public FriendshipController(GenreService genreService, UserService userService, FriendshipService friendshipService, ChatMessageService chatMessageService) {
+        this.genreService = genreService;
+        this.userService = userService;
+        this.friendshipService = friendshipService;
+        this.chatMessageService = chatMessageService;
+
+        this.controllerReusable = new ControllerReusable(userService, friendshipService, chatMessageService);
+    }
 
     @PostMapping("/request/{receiverId}")
     public String sendFriendRequest(@PathVariable Long receiverId, Principal principal) {
@@ -79,6 +94,12 @@ public class FriendshipController {
         model.addAttribute("friendships", friendships);
         model.addAttribute("pendingRequests", pendingRequests);
         model.addAttribute("blockedUsers", blocked);
+
+        List<GenreDto> genreDtos = genreService.findAll();
+        model.addAttribute("genreDtos", genreDtos);
+
+        controllerReusable.addFriendsAttributes(model, principal);
+
         return "friendsList";
     }
 }
